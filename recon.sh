ac9d3a92ef4870edf79198c62e6b61b71ce41e95
s1d6p01nt7 <163605035+s1d6point7bugcrowd@users.noreply.github.com>
2024-05-30 02:48:44 -0400

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -31,8 +31,14 @@ read -p "Enter OOS subdomains and URLs (comma separated): " OOS_INPUT
 # Convert the OOS input to an array
 IFS=',' read -r -a OOS_PATTERNS <<< "$OOS_INPUT"
 
+# Prompt for using Naabu for port scanning
+read -p "Do you want to use Naabu for port scanning? (yes/no): " USE_NAABU
+
 # Prompt for storing files locally
 read -p "Do you want to store files locally? (yes/no): " STORE_LOCALLY
+
+# Prompt for using waybackurls
+read -p "Do you want to use waybackurls? (yes/no): " USE_WAYBACKURLS
 
 # Prompt for using specific Nuclei templates or tags
 read -p "Do you want to use specific Nuclei templates or tags? (yes/no): " USE_SPECIFIC_TEMPLATES_OR_TAGS
@@ -48,7 +54,7 @@ if [ "$USE_SPECIFIC_TEMPLATES_OR_TAGS" == "yes" ]; then
     fi
 else
     # Default Nuclei command options
-    NUCLEI_TEMPLATES_OPTION="-ss template-spray -include-tags misc -etags aem -s critical,high,medium"
+    NUCLEI_TEMPLATES_OPTION="-ss template-spray -include-tags misc -etags aem"
 fi
 
 # Define a function to create files based on user's choice
@@ -61,6 +67,11 @@ create_file() {
     fi
 }
 
+# Function to sanitize URLs for filenames
+sanitize_filename() {
+    echo "$1" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | tr ' ' '_'
+}
+
 # Construct the custom header
 CUSTOM_HEADER="X-Bug-Bounty:researcher@$PROGRAM_NAME"
 echo -e "\033[33mUsing custom header: $CUSTOM_HEADER\033[0m"
@@ -69,7 +80,7 @@ echo -e "\033[33mUsing custom header: $CUSTOM_HEADER\033[0m"
 is_oos() {
     local target=$1
     for pattern in "${OOS_PATTERNS[@]}"; do
-        if [[ "$target" == *"$pattern"* ]] && [[ "$target" != "https://$pattern" ]] && [[ "$target" != "$pattern" ]]; then
+        if [[ "$target" == *"$pattern"* ]]; then
             return 0
         fi
     done
@@ -77,14 +88,18 @@ is_oos() {
 }
 
 # Create necessary files
-SUBS_FILE=$(create_file "${TARGET}-subs.txt")
-FILTERED_SUBS_FILE=$(create_file "${TARGET}-filtered-subs.txt")
-ALIVE_SUBS_IP_FILE=$(create_file "${TARGET}-alive-subs-ip.txt")
-ALIVE_SUBS_FILE=$(create_file "${TARGET}-alive-subs.txt")
-WEB_ALIVE_FILE=$(create_file "${TARGET}-web-alive.txt")
-NUCLEI_READY_FILE=$(create_file "${TARGET}-nuclei-ready.txt")
-NUCLEI_RESULTS_FILE=$(create_file "${TARGET}-nuclei-results.txt")
-SUBZY_RESULTS_FILE=$(create_file "${TARGET}-subzy-results.json")
+SUBS_FILE=$(create_file "$(sanitize_filename "${TARGET}-subs.txt")")
+FILTERED_SUBS_FILE=$(create_file "$(sanitize_filename "${TARGET}-filtered-subs.txt")")
+ALIVE_SUBS_IP_FILE=$(create_file "$(sanitize_filename "${TARGET}-alive-subs-ip.txt")")
+ALIVE_SUBS_FILE=$(create_file "$(sanitize_filename "${TARGET}-alive-subs.txt")")
+OPEN_PORTS_FILE=$(create_file "$(sanitize_filename "${TARGET}-openports.txt")")
+FINAL_OPEN_PORTS_FILE=$(create_file "$(sanitize_filename "${TARGET}-final-openports.txt")")
+WEB_ALIVE_FILE=$(create_file "$(sanitize_filename "${TARGET}-web-alive.txt")")
+NUCLEI_READY_FILE=$(create_file "$(sanitize_filename "${TARGET}-nuclei-ready.txt")")
+NUCLEI_RESULTS_FILE=$(create_file "$(sanitize_filename "${TARGET}-nuclei-results.txt")")
+TEMP_FILE=$(create_file "$(sanitize_filename "temp-file.txt")")
+HTTPS_ONLY_FILE=$(create_file "$(sanitize_filename "https-only.txt")")
+WAYBACK_URLS_FILE=$(create_file "$(sanitize_filename "wayback-urls.txt")")
 
 # Function to remove brackets from IP addresses
 remove_brackets() {
@@ -111,51 +126,78 @@ filter_urls() {
         if is_oos "$url"; then
             echo -e "\033[33mSkipping OOS URL: $url\033[0m"
         else
-            echo "$url" | anew $output_file
+            echo "$url" >> $output_file
         fi
     done < $input_file
     check_file_content $output_file
 }
 
-# Run subfinder, dnsx, and httpx
+# Function to run subfinder, dnsx, and httpx
 run_subfinder_dnsx_httpx() {
     echo -e "\033[33mRunning subfinder...\033[0m"
-    subfinder -d $TARGET -silent -all | anew $SUBS_FILE
+    subfinder -d $TARGET -silent -all > $SUBS_FILE
     check_file_content "$SUBS_FILE"
 
     echo -e "\033[33mFiltering subdomains...\033[0m"
     filter_urls $SUBS_FILE $FILTERED_SUBS_FILE
 
     echo -e "\033[33mRunning dnsx...\033[0m"
-    dnsx -resp -silent < $FILTERED_SUBS_FILE | anew $ALIVE_SUBS_IP_FILE
+    dnsx -resp -a -silent < $FILTERED_SUBS_FILE > $ALIVE_SUBS_IP_FILE
     check_file_content "$ALIVE_SUBS_IP_FILE"
 
-    awk '{print $1}' < $ALIVE_SUBS_IP_FILE | anew $ALIVE_SUBS_FILE
+    awk '{print $1}' < $ALIVE_SUBS_IP_FILE > $ALIVE_SUBS_FILE
     check_file_content "$ALIVE_SUBS_FILE"
 
     echo -e "\033[33mRunning httpx...\033[0m"
-    httpx -silent --rate-limit 5 -title -status-code -mc 200 < $ALIVE_SUBS_FILE | remove_brackets | anew $WEB_ALIVE_FILE
+    httpx -silent --rate-limit 5 -title -status-code -mc 200 < $ALIVE_SUBS_FILE | remove_brackets > $WEB_ALIVE_FILE
     check_file_content "$WEB_ALIVE_FILE"
 
-    # No additional filtering needed, as httpx already filtered status codes 200 and 302
-    cp $WEB_ALIVE_FILE $NUCLEI_READY_FILE
-
-    echo -e "\033[33mRunning subzy for subdomain takeover detection...\033[0m"
-    subzy run --targets $SUBS_FILE --output $SUBZY_RESULTS_FILE
-    check_file_content "$SUBZY_RESULTS_FILE"
+    # Filter for https URLs only
+    grep -E '^https://' $WEB_ALIVE_FILE > $NUCLEI_READY_FILE
+    check_file_content "$NUCLEI_READY_FILE"
 }
 
-# Conditional execution based on mode
+# Function to run waybackurls and analyze output
+run_waybackurls() {
+    local target=$1
+    local wayback_output=$(create_file "$(sanitize_filename "wayback-$target.txt")")
+    echo -e "\033[33mRunning waybackurls for: $target...\033[0m"
+    echo "$target" | waybackurls > "$wayback_output"
+    # Filter the waybackurls output for OOS URLs
+    filter_urls "$wayback_output" $WAYBACK_URLS_FILE
+}
+
+# Main workflow
 if [ "$MODE" == "domain" ]; then
     run_subfinder_dnsx_httpx
 
-    if [ ! -s "$WEB_ALIVE_FILE" ]; then
+    if [ "$USE_WAYBACKURLS" == "yes" ]; then
+        while read -r url; do
+            run_waybackurls "$url"
+        done < $NUCLEI_READY_FILE
+    fi
+
+    if [ "$USE_NAABU" == "yes" ]; then
+        echo -e "\033[33mRunning naabu...\033[0m"
+        sudo naabu -top-ports 1000 -silent < $ALIVE_SUBS_FILE > $OPEN_PORTS_FILE
+        check_file_content "$OPEN_PORTS_FILE"
+
+        cut -d ":" -f1 < $OPEN_PORTS_FILE > $FINAL_OPEN_PORTS_FILE
+        check_file_content "$FINAL_OPEN_PORTS_FILE"
+
+        echo -e "\033[33mRunning httpx on open ports...\033[0m"
+        httpx -silent --rate-limit 5 -title -status-code -mc 200 < $FINAL_OPEN_PORTS_FILE | remove_brackets > $WEB_ALIVE_FILE
+        check_file_content "$WEB_ALIVE_FILE"
+
+        # Filter for https URLs only
+        grep -E '^https://' $WEB_ALIVE_FILE > $NUCLEI_READY_FILE
+        check_file_content "$NUCLEI_READY_FILE"
+    fi
+
+    if [ ! -s "$NUCLEI_READY_FILE" ]; then
         echo -e "\033[33mNo live web targets found by httpx. Exiting.\033[0m"
         exit 1
     fi
-
-    # Remove duplicates
-    sort -u $NUCLEI_READY_FILE -o $NUCLEI_READY_FILE
 
     echo -e "\033[33mURLs being sent to nuclei:\033[0m"
     cat $NUCLEI_READY_FILE
@@ -170,15 +212,22 @@ elif [ "$MODE" == "url" ]; then
         echo -e "\033[33mSkipping OOS URL: $TARGET\033[0m"
     else
         echo -e "\033[33mRunning dnsx...\033[0m"
-        echo "$TARGET" | dnsx -silent | anew $NUCLEI_READY_FILE
+        echo "$TARGET" | dnsx -silent > $NUCLEI_READY_FILE
         check_file_content "$NUCLEI_READY_FILE"
 
         echo -e "\033[33mRunning httpx...\033[0m"
-        cat $NUCLEI_READY_FILE | httpx -silent --rate-limit 5 -title -status-code -mc 200 | anew $WEB_ALIVE_FILE
-        check_file_content "$WEB_ALIVE_FILE"
+        cat $NUCLEI_READY_FILE | httpx -silent --rate-limit 5 -title -status-code -mc 200 > $TEMP_FILE
+        check_file_content "$TEMP_FILE"
 
-        # Remove duplicates
-        sort -u $WEB_ALIVE_FILE -o $NUCLEI_READY_FILE
+        # Filter for https URLs only
+        grep -E '^https://' $TEMP_FILE > $NUCLEI_READY_FILE
+        check_file_content "$NUCLEI_READY_FILE"
+
+        if [ "$USE_WAYBACKURLS" == "yes" ]; then
+            while read -r url; do
+                run_waybackurls "$url"
+            done < $NUCLEI_READY_FILE
+        fi
 
         echo -e "\033[33mURLs being sent to nuclei:\033[0m"
         cat $NUCLEI_READY_FILE
